93970b440d29742977afcc054000eb810f7aaafb
keunbum <rkeunbum@gmail.com>
2022-05-26 23:54:10 +0900

    rebuild kt files

--- app/src/main/java/com/example/kuku/activity/KuIntroActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.kuku.activity;
-
-class KuIntroActivity {
-}
---
+++ app/src/main/java/com/example/kuku/src/activity/KuLocationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kuku.activity
+
+import android.os.Bundle
+import com.example.kuku.databinding.ActivityKuLocationBinding
+
+class KuLocationActivity : KuActivity<ActivityKuLocationBinding>(ActivityKuLocationBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun initLayout() {
+    }
+}
