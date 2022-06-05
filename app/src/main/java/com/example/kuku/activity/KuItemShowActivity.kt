07ef87a59e290013ed551a5d0444e674c490a562
keunbum <rkeunbum@gmail.com>
2022-06-05 15:21:52 +0900

    update KuSearchActivity and KuItemShowActivity 준영님이랑 호철님이 올려주신 파일들 합쳐서 만들었습니다.

--- app/src/main/java/com/example/kuku/activity/KuItemShowActivity.kt
+++ app/src/main/java/com/example/kuku/activity/KuItemShowActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kuku.activity
 
+import android.content.Intent
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.kuku.data.KuData
 import com.example.kuku.databinding.ActivityKuItemShowBinding
 
 class KuItemShowActivity : KuActivity<ActivityKuItemShowBinding>(ActivityKuItemShowBinding::inflate) {
@@ -9,5 +12,30 @@ class KuItemShowActivity : KuActivity<ActivityKuItemShowBinding>(ActivityKuItemS
     }
 
     override fun init() {
+        initLayout()
+    }
+
+    private fun initLayout() {
+        val data = intent.getSerializableExtra("data") as KuData
+        binding.productProductNameTv.text = data.name // 제품명
+        binding.productDetailTv.text = data.description //제품설명
+        binding.productPriceTv.text = data.price.toString() + "원" //제품가격
+        binding.productStockTv.text = "재고: " + data.stock.toString() + "개" //제품재고
+        Glide.with(binding.root).load(data.imgUrl).into(binding.productIv) //이미지
+
+        binding.productBackIv.setOnClickListener {
+            val intent = Intent(this, KuSearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.searchLocBtn.setOnClickListener {
+//            val intent = Intent(this, KuLocationActivity::class.java)
+//            startActivity(intent)
+        }
+
+        binding.addBasketBtn.setOnClickListener {
+//            val intent = Intent(this, KuBasketActivity::class.java)
+//            startActivity(intent)
+        }
     }
 }
