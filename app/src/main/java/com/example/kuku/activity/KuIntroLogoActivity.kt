3ae5080de7e71c5a81d78e1eff855b0bbcc19cd5
keunbum <rkeunbum@gmail.com>
2022-06-09 00:00:04 +0900

    update KuIntroLogoActivity * hochul branch를 기반으로 하되 * 처음 APK를 설치하는 디바이스를 위해 loadDB 구현 * 앱 실행 시간 3초 -> 2초로 단축(크게 중요한 요소는 X)

--- app/src/main/java/com/example/kuku/activity/KuIntroLogoActivity.kt
+++ app/src/main/java/com/example/kuku/activity/KuIntroLogoActivity.kt
@@ -1,28 +1,56 @@
 package com.example.kuku.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.core.content.ContextCompat.startActivity
-import com.example.kuku.R
-import com.example.kuku.databinding.ActivityKuIntroBinding
 import com.example.kuku.databinding.ActivityKuIntroLogoBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class KuIntroLogoActivity : KuActivity<ActivityKuIntroLogoBinding>(ActivityKuIntroLogoBinding::inflate) {
-    private val WAIT_TIME = 3000L
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val WAIT_TIME = 2000L
 
     override fun init() {
+        loadDB("kuku.db")
+        logoDelay()
+    }
+
+    private fun logoDelay() {
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
             val intent = Intent(this, KuIntroActivity::class.java)
             startActivity(intent)
         }, WAIT_TIME)
+    }
+
+    private fun loadDB(DB_NAME: String) {
+        val DB_PATH = "/data/data/" + applicationContext.packageName + "/databases/"
+        try {
+            val fileDir = File(DB_PATH)
+            if (!fileDir.exists()) {
+                fileDir.mkdir()
+            }
+            val strOutFile = DB_PATH + DB_NAME
+            val dbFile = File(strOutFile)
+            if (dbFile.exists()) {
+//                System.out.println("dbFile exists")
+                return
+            }
+//            System.out.println("dbFile doesn't exist")
+            val inputStream = applicationContext.assets.open(DB_NAME)
+            val outputStream = FileOutputStream(strOutFile)
+
+            val mBuffer = ByteArray(1024)
+            var mLength: Int
+            while ((inputStream.read(mBuffer).also { mLength = it }) > 0) {
+                outputStream.write(mBuffer,0, mLength)
+            }
+            outputStream.flush()
+            outputStream.close()
+            inputStream.close()
+        } catch(e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     override fun onPause() {
