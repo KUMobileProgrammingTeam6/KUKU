b706814f33a429c8a7980ee113cb1d4dcc9612ae
keunbum <rkeunbum@gmail.com>
2022-06-08 23:57:33 +0900

    update KuLocation feature * 버그: 실제 디바이스에서 빨강 기호가 의도한 대로 배치가 안됨. * prodcutBackIv에 onBackPressed() 추가

--- app/src/main/java/com/example/kuku/activity/KuLocationActivity.kt
+++ app/src/main/java/com/example/kuku/activity/KuLocationActivity.kt
@@ -1,13 +1,30 @@
 package com.example.kuku.activity
 
-import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.ImageView
+import com.example.kuku.data.KuData
 import com.example.kuku.databinding.ActivityKuLocationBinding
 
 class KuLocationActivity : KuActivity<ActivityKuLocationBinding>(ActivityKuLocationBinding::inflate) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var imageViews: Array<ImageView>
 
     override fun init() {
+        initLayout()
     }
+
+    private fun initLayout() {
+        imageViews = with(binding) {arrayOf(imageViewA, imageViewB, imageViewC, imageViewD, imageViewE, imageViewF, imageViewG)}
+        val items = intent.getSerializableExtra("items") as ArrayList<KuData>
+        for (item in items) {
+            val pos = locToPos(item.location)
+            imageViews[pos].visibility = View.VISIBLE
+        }
+
+        binding.productBackIv.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun locToPos(location: Int) = location / 100 - 1
 }
